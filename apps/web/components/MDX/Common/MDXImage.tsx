8a253dc333592e13ca0855959443d9364223da0c
Zhen Jun Xu <arlenxuzj@gmail.com>
2023-01-28 10:14:13 -0800

    fix(web): mdx image zoom in zoom out

--- apps/web/components/MDX/Common/MDXImage.tsx
+++ apps/web/components/MDX/Common/MDXImage.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
+import { useState } from 'react';
 
-import { Box } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import { Box, Modal } from '@mui/material';
 
 export type MDXImageProps = {
   src?: string;
@@ -16,6 +19,7 @@ export type MDXImageProps = {
 const MDXImage = (props: MDXImageProps) => {
   const { src, alt, renderedWidth, originWidth, maxRenderWidth, originRatio } =
     props;
+  const [open, setOpen] = useState(false);
 
   if (!src) {
     return null;
@@ -33,26 +37,79 @@ const MDXImage = (props: MDXImageProps) => {
     }
   }
 
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Box
-      sx={{
-        width: '100%',
-        mb: 5,
-        display: 'flex',
-        justifyContent: 'center'
-      }}
-    >
-      <Image
-        src={src}
-        alt={alt || 'image'}
-        width={width}
-        height={Math.round(width / Number(originRatio))}
-        style={{
-          maxWidth: '100%',
-          height: 'auto'
+    <>
+      <Box
+        sx={{
+          width: '100%',
+          mb: 5,
+          display: 'flex',
+          justifyContent: 'center',
+          cursor: 'zoom-in'
         }}
-      />
-    </Box>
+        onClick={handleClick}
+      >
+        <Image
+          src={src}
+          alt={alt || 'image'}
+          width={width}
+          height={Math.round(width / Number(originRatio))}
+          style={{
+            maxWidth: '100%',
+            height: 'auto'
+          }}
+        />
+      </Box>
+      <Modal open={open} onClose={handleClose} disableAutoFocus>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: '100%',
+            cursor: 'pointer'
+          }}
+          onClick={handleClose}
+        >
+          <Box
+            sx={{
+              position: 'absolute',
+              display: 'flex',
+              width: '100%',
+              top: 0,
+              justifyContent: 'flex-end'
+            }}
+          >
+            <Box
+              sx={{
+                p: 2
+              }}
+            >
+              <CloseIcon fontSize='large' />
+            </Box>
+          </Box>
+          <img
+            src={src}
+            alt={alt || 'image'}
+            style={{
+              maxWidth: '80vw',
+              height: 'auto',
+              maxHeight: '80vh',
+              cursor: 'zoom-out'
+            }}
+          />
+        </Box>
+      </Modal>
+    </>
   );
 };
 
